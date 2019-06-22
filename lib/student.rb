0d3d5400920344f35d977d90cad8672f26cb0e5e
Lauren McCoy <lmccoy8911@gmail.com>
2019-06-22 20:58:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
   def self.create(name:,grade:)
     student = Student.new(name, grade)
     student.save
-    student(name,grade) 
+    student
     DB[:conn].execute(sql,self.name,self.grade)
   end 
 end
